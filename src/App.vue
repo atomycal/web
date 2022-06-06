a68601d63d2403d47efc318f06edff91b61fcfab
Ruben Tan <soggie@gmail.com>
2022-06-07 00:18:40 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,30 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import SiteMenu from './components/SiteMenu.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <site-menu></site-menu>
+  <h1>Something explosive is coming</h1>
 </template>
 
-<style>
+<style lang="scss">
+body, html {
+  margin: 0;
+  font-size: 16px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Mulish, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.button {
+  font-family: Montserrat, Helvetica, sans-serif;
+  font-weight: bold;
 }
 </style>
---
+++ src/components/SiteMenu.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const isShowOverlay = ref(false)
+const overlayDisplay = computed(() => isShowOverlay.value === true ? 'block' : 'none')
+
+const siteMenuItems = ref([
+	{
+		name: 'Home',
+		to: '/'
+	},
+
+	{
+		name: 'Portfolio',
+		to: '/portfolio'
+	},
+
+	{
+		name: 'Services',
+		to: '/services'
+	},
+
+	{
+		name: 'Contact Us',
+		to: '/contact-us'
+	}
+])
+</script>
+
+<template>
+	<header class="am-header" role="header">
+		<div class="am-overlay"></div>
+
+		<div class="am-nav-main">
+			<div class="am-sale-main">
+				<a class="button" href="#">-</a>
+			</div>
+
+			<div class="am-brand-main">-</div>
+
+			<div class="am-menu-main">-</div>
+		</div>
+	</header>
+</template>
+
+<style scoped lang="scss">
+.am-header {
+	z-index: 5000;
+	background-color: #000;
+}
+
+.am-overlay {
+	position: fixed;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	background-color: rgba(0, 0, 0, 0.8);
+	display: v-bind(overlayDisplay);
+}
+
+.am-nav-main {
+	display: flex;
+	justify-content: space-between;
+
+	color: white;
+	padding: 1rem;
+}
+
+.am-sale-main {
+	.button {
+		text-decoration: none;
+
+		background-color: #AABD8C;
+		color: #191308;
+		padding: .5rem 1rem;
+		border-radius: .25rem;
+	}
+}
+</style>
